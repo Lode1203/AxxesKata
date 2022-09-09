7dfc2f40a49f0983c80c6ae6a5c5d8f6599fa881
HatzuBatzzu <102261752+HatzuBatzzu@users.noreply.github.com>
2022-09-09 21:14:47 +0200

    Added the derived classes and interface
    Mainly looked over the code to see how i would implement the different behaviours

---
+++ CSharp/GildedTros.App/Items/ItemFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GildedTros.App.Items
+{
+    /// <summary>
+    /// Handles the creation of the different types of possible items, this to handle the separation of the different possible behaviours in the derived classes
+    /// </summary>
+    public static class ItemFactory
+    {
+        public static Item CreateAppreciatingItem(string name, uint sellIn, uint quality, uint appreciationRate)
+        {
+            return null;
+        }
+
+        public static Item CreateDegradingItem(string name, uint sellIn, uint quality, uint degradationRate)
+        {
+            return null;
+        }
+
+        public static Item CreateLegendaryItem(string name, uint sellIn, uint quality)
+        {
+            return null;
+        }
+    }
+}
